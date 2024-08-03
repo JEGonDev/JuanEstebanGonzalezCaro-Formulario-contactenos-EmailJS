6e863dcc23d4758d5db0a32f81f84a83d83cafe0
JuanitoZaZa <juanestebangonzalezcaro567@gmail.com>
2024-08-02 20:43:50 -0500

    Se corrigio errores de los parametros del metodo sendform y se cambio la forma de inicializar emailjs directamente desde el index.html

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
+//Se inicializa EmailJS en un script del index.html especificamente la linea 10
+
+const button = document.getElementById('button-submit'); //Se crea una constante para que el boton pueda ser escuchado
 const form = document.getElementById('form');
 const serviceID = "service_bhastph"; //ID del servicio de emailing
 const templateID = "template_1q2jifo"; // ID del templaete de emailing
-const apiKey = "g4gjsQmQHPVVmvgNI"; //Llave publica
 
-form.addEventListener('submit', submitEmail) //Añade el oyente al boton enviar
+button.addEventListener('click', submitEmail); //Se agrega un evento al boton para que se ejecute la funcion submitEmail
 
-function submitEmail(e) {
-    e.preventDefault(); 
-    emailjs.init(serviceID);
+function submitEmail(event) {
+    event.preventDefault(); 
+    // emailjs.init(serviceID); No hay necesidad de inicializar emailjs en el script ya que ya se hizo en el index.html
 
-    emailjs.sendForm(serviceID, templateID)
-    .then(result => {
+    emailjs.sendForm(serviceID, templateID, form).then(() => {
+        button.value = 'Email enviado';
         Swal.fire('Gracias por contactarnos!', 'Nos pondremos en contacto con usted a la brevedad.', 'success');
-    })
-    .catch((error) => {
+    }).catch((err) => { // Cambié el segundo parámetro a `catch` para un manejo de errores más claro
+        button.value = 'Error al enviar';
         Swal.fire({
             icon: 'error',
             title: 'Oops...',
